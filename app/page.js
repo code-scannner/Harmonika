ec75af3a37fce72a7b4bdf6c7afc540ca4255dce
code-scanner <cyber.gupta001@gmail.com>
2024-07-16 15:22:03 +0530

    more songs with new build

--- app/page.js
+++ app/page.js
@@ -13,11 +13,7 @@ export default function Home() {
       <div className='filter blur-[10px] absolute inset-0 w-full -z-10'
         style={{ backgroundImage: `url('${backgrounds[0]}')` }}
       ></div>
-        <Main />
-      {/* {curr.length == 0 && <section className="fixed right-0 z-10 border border-solid border-gray-700 rounded-s-md">
-        {/* <button onClick={()=>setCurr('')} className="bg-gray text-white absolute -left-2 -top-2">x</button> */}
-      {/* <iframe className="rounded-s-md" width={250} height={141} src="https://www.youtube.com/embed/v4h5iPlxj0c?si=Xm4XXTqzJylS1fvY&controls=0&autoplay=true" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen /> */}
-      {/* </section>} */}
+      <Main />
       <Label />
     </main>
   );
